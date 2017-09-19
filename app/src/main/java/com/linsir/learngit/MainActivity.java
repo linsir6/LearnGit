285318e36818d7c86bd210b5edf99f74c8a83212
linsir <linsir678@gmail.com>
2017-09-19 21:07:46 +0800

    test

--- app/src/main/java/com/linsir/learngit/MainActivity.java
+++ app/src/main/java/com/linsir/learngit/MainActivity.java
@@ -2,6 +2,8 @@ package com.linsir.learngit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("lin","现在是版本1");
     }
 }
